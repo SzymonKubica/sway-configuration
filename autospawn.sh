e4636b997db2fd879a777fd71a1949c9ae621442
SzymonKubica <szymo.kubica@gmail.com>
2023-01-28 18:44:26 +0000

    Added an autostart script for the most common setup.

---
+++ autospawn.sh
@@ -0,0 +1,18 @@
+
+swaymsg exec exec -- alacritty --hold -e zsh -c neofetch &&
+sleep 0.1 &&
+swaymsg exec exec -- alacritty --hold -e zsh -c ncmpcpp &&
+sleep 0.1 &&
+swaymsg exec exec -- alacritty --hold -e zsh -c btop &&
+sleep 0.1 &&
+swaymsg focus left &&
+swaymsg focus left &&
+swaymsg focus left &&
+swaymsg move scratchpad &&
+swaymsg layout toggle split &&
+swaymsg focus down &&
+swaymsg focus down &&
+swaymsg move right
+
+
+
